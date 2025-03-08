2647defcca8348677906c439912b1fa62ca1b415
useltonputman810 <useltonputman810@gmail.com>
2025-03-08 20:18:21 +0000

    Add 3.js

---
+++ src/3.js
@@ -0,0 +1,7 @@
+// Code here
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let randomNum = getRandomNumber(1, 10);
+console.log(randomNum);
