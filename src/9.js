94f092878ef71fdd3ddff92513bd2d9f60035fe4
useltonputman810 <useltonputman810@gmail.com>
2025-03-14 20:45:37 +0000

    Add 9.js

---
+++ src/9.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Print the generated number
+console.log(`The randomly generated number is: ${randomNumber}`);
