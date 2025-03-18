7f693cb955e9ba3f3b915a366db0ae26fc5c191c
useltonputman810 <useltonputman810@gmail.com>
2025-03-18 11:07:53 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,8 @@
+let a = Math.random() * 10;
+if (a < 5) {
+  console.log("The number is less than 5");
+} else if (a >= 5 && a <= 8) {
+  console.log("The number is between 5 and 8");
+} else {
+  console.log("The number is greater than 8");
+}
